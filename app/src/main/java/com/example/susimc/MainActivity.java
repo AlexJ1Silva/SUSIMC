8c47058a6c9274b4be1908327f73624348fc9df5
Alex J. Silva <111905692+AlexJ1Silva@users.noreply.github.com>
2023-10-24 14:16:06 -0300

    Correção das condições e layout

--- app/src/main/java/com/example/susimc/MainActivity.java
+++ app/src/main/java/com/example/susimc/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.susimc;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,64 +33,96 @@ import androidx.appcompat.app.AppCompatActivity;
                     calcular();
                 }
             });
+
+            Button limparButton = findViewById(R.id.limpar);
+            limparButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    limparDados();
+                }
+            });
+
         }
 
+
+        // funcção para calcular o IMC
         private void calcular() {
+
+
             String nome = nomeEditText.getText().toString();
-            double peso = Double.parseDouble(pesoEditText.getText().toString());
-            double altura = Double.parseDouble(alturaEditText.getText().toString());
-            double imc = peso / (altura * altura);
+            String pesoStr = pesoEditText.getText().toString();
+            String alturaStr = alturaEditText.getText().toString();
 
             if (nome.isEmpty()) {
                 respostaTextView.setTextColor(getResources().getColor(R.color.red));
                 respostaTextView.setText("[ERRO] Você não colocou o seu nome!");
-
-            }else if (peso == 0){
+            } else if (pesoStr.isEmpty()) {
+                respostaTextView.setTextColor(getResources().getColor(R.color.red));
                 respostaTextView.setText("[ERRO] Você não colocou o seu peso!");
-            }else if (altura == 0){
+            }else if (alturaStr.isEmpty()) {
+                respostaTextView.setTextColor(getResources().getColor(R.color.red));
                 respostaTextView.setText("[ERRO] Você não colocou a sua altura!");
-            }else if (imc < 16) {
-                respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau magreza III.");
-                img.setImageResource(R.drawable.magreza3);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorMagreza3));
-            } else if (imc > 16.1 && imc < 16.9) {
-                respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau magreza II.");
-                img.setImageResource(R.drawable.magreza2);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorMagreza2));
-            } else if (imc > 17 && imc < 18.4) {
-                respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau magreza I.");
-                img.setImageResource(R.drawable.magreza1);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorMagreza1));
-            } else if (imc > 18.5 && imc < 24.9) {
-                respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está com o peso normal.");
-                img.setImageResource(R.drawable.normal);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorNormal));
-            } else if (imc > 25 && imc < 29.9) {
-                respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está com sobrepeso.");
-                img.setImageResource(R.drawable.sobrepeso);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorSobrepeso));
-            } else if (imc > 30 && imc < 34.9) {
-                respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau obesidade I.");
-                img.setImageResource(R.drawable.obesidade1);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorObesidade1));
-            } else if (imc > 35 && imc < 39.9) {
-                respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau obesidade II.");
-                img.setImageResource(R.drawable.obesidade2);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorObesidade2));
-            } else if (imc > 40) {
-                respostaTextView.setText(nome + " Seu IMC é " + String.format("%.2f", imc) + " você está no grau obesidade III.");
-                img.setImageResource(R.drawable.obesidade3);
-                getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorObesidade3));
+            }else if (!alturaStr.contains(".")) {
+                respostaTextView.setTextColor(getResources().getColor(R.color.red));
+                respostaTextView.setText("[ERRO] A altura deve conter ponto (exemplo: 1.82)!");
+            }else {
+                try {
+                    respostaTextView.setTextColor(getResources().getColor(R.color.black));
+                    double peso = Double.parseDouble(pesoStr);
+                    double altura = Double.parseDouble(alturaStr);
+                    double imc = peso / (altura * altura);
+                    respostaTextView.setText("[ERRO] Você não colocou o seu peso!");
+                    if (altura == 0) {
+                        respostaTextView.setText("[ERRO] Você não colocou a sua altura!");
+                    } else if (imc < 16) {
+                        respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau magreza III.");
+                        img.setImageResource(R.drawable.magreza3);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorMagreza3));
+                    } else if (imc > 16.1 && imc < 16.9) {
+                        respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau magreza II.");
+                        img.setImageResource(R.drawable.magreza2);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorMagreza2));
+                    } else if (imc > 17 && imc < 18.4) {
+                        respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau magreza I.");
+                        img.setImageResource(R.drawable.magreza1);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorMagreza1));
+                    } else if (imc > 18.5 && imc < 24.9) {
+                        respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está com o peso normal.");
+                        img.setImageResource(R.drawable.normal);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorNormal));
+                    } else if (imc > 25 && imc < 29.9) {
+                        respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está com sobrepeso.");
+                        img.setImageResource(R.drawable.sobrepeso);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorSobrepeso));
+                    } else if (imc > 30 && imc < 34.9) {
+                        respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau obesidade I.");
+                        img.setImageResource(R.drawable.obesidade1);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorObesidade1));
+                    } else if (imc > 35 && imc < 39.9) {
+                        respostaTextView.setText(nome + " Seu imc é " + String.format("%.2f", imc) + " você está no grau obesidade II.");
+                        img.setImageResource(R.drawable.obesidade2);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorObesidade2));
+                    } else if (imc > 40) {
+                        respostaTextView.setText(nome + " Seu IMC é " + String.format("%.2f", imc) + " você está no grau obesidade III.");
+                        img.setImageResource(R.drawable.obesidade3);
+                        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorObesidade3));
+                    }
+                } catch (NumberFormatException e) {
+                    respostaTextView.setText("[ERRO] Peso e altura devem ser números válidos!");
+                }
             }
         }
-        private void limpar(){
-            String nome = "";
-            double peso = 0;
-            double altura = 0;
-            double imc = 0;
-            String respostaTextView = "";
-
+        
+        // função para limpar os campos
+        private void limparDados() {
+            nomeEditText.setText("");  // limpa o campo de nome
+            pesoEditText.setText("");  // limpa o campo de peso
+            alturaEditText.setText("");  // limpa o campo de altura
+            respostaTextView.setText("");  // limpa o campo de resposta
+            img.setImageResource(0);  // remove a imagem
+            getWindow().getDecorView().setBackgroundColor(getResources().getColor(android.R.color.white));  // restaura a cor de fundo padrão
         }
+
 
     }
 
